28e6f10102a69e38ce09cb798f03cce70ef9a1c7
Emilio Girardi <64197141+girardi-e@users.noreply.github.com>
2021-08-23 19:12:51 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'rest-client'
+require 'json'
+
+url = ('https://randomuser.me/api/?results=25&inc=name,email,phone,login,picture&nat=us')
+
+response = RestClient.get(url)
+repos = JSON.parse(response)['results']
+
+repos.each do |person|
+  file = URI.open('https://thispersondoesnotexist.com/image')
+  new_user = Buddy.create(first_name: person['name']['first'],
+                          last_name: person['name']['last'],
+                          address: person['street']['number']['name']),
+                          email: person['email'],
+                          password: person['password']
+  new_user.photo.attach(io: file, filename: 'new_user.jpg', content_type: 'image/jpg')
+end

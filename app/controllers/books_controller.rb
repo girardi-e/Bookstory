a30f78f08bde88a85bd1347d7ebfc8bf58947962
Emilio Girardi <64197141+girardi-e@users.noreply.github.com>
2021-08-25 18:32:07 +0200

    my orders table

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -44,7 +44,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :author, :description, :price)
+    params.require(:book).permit(:title, :author, :description, :price, :photo)
   end
 
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :user
   validates :title, :author, :price, presence: true
+
+  has_one_attached :photo
 end

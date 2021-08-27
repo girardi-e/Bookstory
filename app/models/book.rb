529ffd3eaab3ef2c55fa00c0cfe86a7434a4a63f
Emilio Girardi <64197141+girardi-e@users.noreply.github.com>
2021-08-27 18:48:33 +0200

    db seed

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
   validates :title, :author, :price, presence: true
-  validates :description, length: { maximum: 100}
+  validates :description, length: { maximum: 200 }
 
   has_one_attached :photo
 end

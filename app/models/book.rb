97f31b178b06e5e06e88f5d8c6c2fd06e90bde9e
Lucas Martelli <lucas98martelli@hotmail.com>
2021-08-27 12:42:50 -0300

    description and photo fixes

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
   validates :title, :author, :price, presence: true
+  validates :description, length: { maximum: 100}
 
   has_one_attached :photo
 end
